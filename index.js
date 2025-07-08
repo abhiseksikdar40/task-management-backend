a02e260667aabb1bc0bced1a73f1c7faca674bb9
Abhisek Sikdar <abhiseksikdar40@gmail.com>
2025-07-08 20:38:45 +0530

    fix: fixed vercel problem.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const serverless = require('serverless-http');
 const app = express()
 const cors = require('cors')
 const JWT = require('jsonwebtoken')
@@ -59,3 +60,5 @@ app.post('/v1/signup/user', async (req, res) => {
 });
 
 
+module.exports = app;
+module.exports.handler = serverless(app);
